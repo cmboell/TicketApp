f7e0bee3a0fc9ae570660eb576e56bfd5e8f9b12
Colby Boell <cmboell1221@gmail.com>
2021-07-11 22:50:34 -0500

    Added

--- TicketApp/Models/Ticket.cs
+++ TicketApp/Models/Ticket.cs
@@ -24,13 +24,15 @@ namespace TicketApp.Models
         [Range(1, 10, ErrorMessage = "Point value must be between 1 - 10")]//sets a range that number has to be in
         public int? PointValue { get; set; }
         [Required(ErrorMessage = "Please enter a deadline.")]//makes it required/custom error message
-        [Range(typeof(DateTime),"1/1/2000","12/31/2099",ErrorMessage ="Deadline must be after 1/1/2000 and cannot exceed 12/31/2099")]//sets date range
+        [Range(typeof(DateTime), "1/1/2000", "12/31/2099", ErrorMessage = "Deadline must be after 1/1/2000 and cannot exceed 12/31/2099")]//sets date range
         public DateTime? Deadline { get; set; }
-        [Required(ErrorMessage = "Please enter a sprint number for your ticket.")]
+        [Required(ErrorMessage = "Please select a sprint number for your ticket.")]
+        [Phone]
         public string SprintId { get; set; }
         public Sprint Sprint { get; set; }
 
         [Required(ErrorMessage = "Please select a status.")]
+        [RegularExpression("^[a-zA-Z0-9]+$")]
         public string StatusId { get; set; }
         public Status Status { get; set; }
 
