d9a40669a7c6fdb5d295bbb8c1d9fd656ac89751
Colby Boell <cmboell1221@gmail.com>
2021-07-24 17:40:28 -0500

    added for assignment

---
+++ TicketApp/Models/StatusViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TicketApp.Models;
+
+namespace TicketApp.Models
+{
+    public class StatusViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(Ticket ticket)
+        {
+            return View(ticket);
+        }
+    }
+}
+
