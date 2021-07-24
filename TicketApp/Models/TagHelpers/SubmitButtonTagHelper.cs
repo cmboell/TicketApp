c3246ce3d0ebf9434491c1eed95696daf2db4998
Colby Boell <cmboell1221@gmail.com>
2021-07-24 15:34:42 -0500

    add taghelper for submit button

---
+++ TicketApp/Models/TagHelpers/SubmitButtonTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+//custom tag helper that stylizes the submit buttons
+namespace TicketApp.Models
+{
+    [HtmlTargetElement(Attributes = "[type=submit]")]
+    public class SubmitButtonTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context,
+  TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("class", "btn btn-primary btn-small");
+
+        }
+    }
+}
